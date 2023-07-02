7912bb4301e7220f86f8ecbb94c69baf7fe45fd5
algernon-A <aeneas@aloku.net>
2023-07-02 12:31:46 +1000

    Add support for editors.

--- Ultimate Eyecandy/LuminaMod/Loading.cs
+++ Ultimate Eyecandy/LuminaMod/Loading.cs
@@ -1,14 +1,19 @@
 ﻿namespace Lumina
 {
+    using System.Collections.Generic;
     using AlgernonCommons.Patching;
     using ICities;
-    using UnityEngine;
 
     /// <summary>
     /// Main loading class: the mod runs from here.
     /// </summary>
     public sealed class Loading : PatcherLoadingBase<OptionsPanel, PatcherBase>
     {
+        /// <summary>
+        /// Gets a list of permitted loading modes.
+        /// </summary>
+        protected override List<AppMode> PermittedModes => new List<AppMode> { AppMode.Game, AppMode.MapEditor, AppMode.AssetEditor, AppMode.ThemeEditor, AppMode.ScenarioEditor };
+
         /// <summary>
         /// Called by the game when exiting a level.
         /// </summary>
