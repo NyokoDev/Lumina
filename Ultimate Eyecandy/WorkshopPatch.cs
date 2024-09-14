19f898e8ce925db95512a82969d4a9ad7341c42b
NyokoDev <129089982+NyokoDev@users.noreply.github.com>
2024-09-14 12:32:35 -0600

    Current build

--- Ultimate Eyecandy/WorkshopPatch.cs
+++ Ultimate Eyecandy/WorkshopPatch.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
-using AlgernonCommons;
 using System.Linq;
+using AlgernonCommons;
 using ColossalFramework.PlatformServices;
 using HarmonyLib;
 
@@ -21,97 +21,97 @@ namespace Lumina.Patching
         }
     }
 
-    [HarmonyPatch(typeof(WorkshopModUploadPanel), "PrepareStagingArea")]
-    public static class StagingPatch
-    {
-        public static void Postfix()
+        [HarmonyPatch(typeof(WorkshopModUploadPanel), "PrepareStagingArea")]
+        public static class StagingPatch
         {
-            string folders = string.Join("\\", new[] { "Colossal Order", "Cities_Skylines", "WorkshopStagingArea" });
-
-            string workshopStagingAreaPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), folders);
-            Logger.Log(workshopStagingAreaPath);
-
-            // Get the latest directory based on creation time
-            DirectoryInfo latestDirectory = new DirectoryInfo(workshopStagingAreaPath)
-                .GetDirectories()
-                .OrderByDescending(d => d.CreationTime)
-                .FirstOrDefault();
-
-            if (latestDirectory != null)
+            public static void Postfix()
             {
-                string inPath = Path.Combine(latestDirectory.FullName, "Content");
-                if (File.Exists(Path.Combine(inPath, ".style")))
+                // Define the base path and subfolders
+                string subfolders = CombinePaths("Colossal Order", "Cities_Skylines", "WorkshopStagingArea");
+                string workshopStagingAreaPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), subfolders);
+                Logger.Log("Workshop Staging Area Path: " + workshopStagingAreaPath);
+
+                // Ensure the directory exists
+                if (!Directory.Exists(workshopStagingAreaPath))
                 {
-                    // Check if workshopStagingAreaPath is null or empty
-                    if (string.IsNullOrEmpty(workshopStagingAreaPath))
-                    {
-                        Logger.Log("Invalid Workshop Staging Area path.");
-                        return;
-                    }
-
-                    // Check if the directory exists
-                    if (!Directory.Exists(workshopStagingAreaPath))
-                    {
-                        Logger.Log("Workshop Staging Area path not found.");
-
-                        // Workaround: Create the directory
-                        try
-                        {
-                            Directory.CreateDirectory(workshopStagingAreaPath);
-                            Logger.Log("Workshop Staging Area path created: " + workshopStagingAreaPath);
-                        }
-                        catch (Exception ex)
-                        {
-                            Logger.Log("Error creating Workshop Staging Area path: " + ex.Message);
-                            return;
-                        }
-                    }
-
-                    Logger.Log(workshopStagingAreaPath);
-
-
-                    // Get the latest directory based on creation time
-                    DirectoryInfo latestDirectoryOm = new DirectoryInfo(workshopStagingAreaPath)
-                        .GetDirectories()
-                        .OrderByDescending(d => d.CreationTime)
-                        .FirstOrDefault();
-
-                    if (latestDirectory == null)
-                    {
-                        Logger.Log("No directories found in Workshop Staging Area.");
-                        return;
-                    }
-
-                    // Path to the initial image (ThemeMix.png)
-                    string folders2 = string.Join("\\", new[] { "Resources", "PreviewImage.png" });
-                    Logger.Log(folders2);
-                    string initialImagePath = Path.Combine(AssemblyUtils.AssemblyPath, folders2);
-                    Logger.Log(initialImagePath);
-                    Logger.Log("Initial path = " + initialImagePath);
-
-                    // Copy the initial image into the latest directory
                     try
                     {
-                        string destinationImagePath = Path.Combine(latestDirectory.FullName, "PreviewImage.png");
-                        File.Copy(initialImagePath, destinationImagePath, true);
-                        Logger.Log("Initial image (PreviewImage.png) added successfully.");
+                        Directory.CreateDirectory(workshopStagingAreaPath);
+                        Logger.Log("Created Workshop Staging Area path: " + workshopStagingAreaPath);
                     }
                     catch (Exception ex)
                     {
-                        Logger.Log($"Failed to copy initial image (PreviewImage.png): {ex.Message}");
+                        Logger.Log("Error creating Workshop Staging Area path: " + ex.Message);
+                        return;
+                    }
+                }
+
+                // Get the latest directory based on creation time
+                DirectoryInfo latestDirectory = new DirectoryInfo(workshopStagingAreaPath)
+                    .GetDirectories()
+                    .OrderByDescending(d => d.CreationTime)
+                    .FirstOrDefault();
+
+                if (latestDirectory != null)
+                {
+                    string contentDirectory = Path.Combine(latestDirectory.FullName, "Content");
+                    Logger.Log("Content Directory: " + contentDirectory);
+
+                    // Check if the Content directory exists and search for .light files
+                    if (Directory.Exists(contentDirectory))
+                    {
+                        string[] lightFiles = Directory.GetFiles(contentDirectory, "*.light");
+
+                        if (lightFiles.Length > 0)
+                        {
+                            // Path to the initial image
+                            string resourcesDirectory = Path.Combine(AssemblyUtils.AssemblyPath, "Resources");
+                            string initialImagePath = Path.Combine(resourcesDirectory, "PreviewImage.png");
+                            Logger.Log("Initial image path: " + initialImagePath);
+
+                            // Copy the initial image into the latest directory
+                            try
+                            {
+                                string destinationImagePath = Path.Combine(latestDirectory.FullName, "PreviewImage.png");
+                                File.Copy(initialImagePath, destinationImagePath, true);
+                                Logger.Log("Initial image (PreviewImage.png) added successfully.");
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Log($"Failed to copy initial image (PreviewImage.png): {ex.Message}");
+                            }
+                        }
+                        else
+                        {
+                            Logger.Log("No .light file found in Content directory.");
+                        }
+                    }
+                    else
+                    {
+                        Logger.Log("Content directory does not exist.");
                     }
                 }
                 else
                 {
-                    Logger.Log("Not a style?");
+                    Logger.Log("No directories found in Workshop Staging Area.");
                 }
             }
-            else
+
+
+
+
+
+            public static string CombinePaths(string first, params string[] others)
             {
-                // Handle case when no directories are found
+                // Put error checking in here :)
+                string path = first;
+                foreach (string section in others)
+                {
+                    path = Path.Combine(path, section);
+                }
+                return path;
             }
-
-
         }
     }
-}
+
+
